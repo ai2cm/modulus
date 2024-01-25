81893391dce6f76205f955f10f97ef132ae6409e
brianhenn <brianhenn@allenai.org>
2024-01-25 23:04:40 +0000

    amend above to avoid call only if specified

--- modulus/models/sfno/initialization.py
+++ modulus/models/sfno/initialization.py
@@ -46,14 +46,10 @@ def _no_grad_trunc_normal_(tensor, mean, std, a, b, inplace_erfinv=True):
         logging.info("finished tensor.uniform_")
         # # Use inverse cdf transform for normal distribution to get truncated
         # # standard normal
-        # if inplace_erfinv:
-        #     logging.info("starting tensor.erfinv_()")
-        #     tensor.erfinv_()
-        #     logging.info("finished tensor.erfinv_()")
-        # else:
-        #     logging.info("starting tensor = torch.erfinv()")
-        #     tensor = torch.erfinv(tensor)
-        #     logging.info("finished tensor = torch.erfinv()")
+        if inplace_erfinv:
+            logging.info("starting tensor.erfinv_()")
+            tensor.erfinv_()
+            logging.info("finished tensor.erfinv_()")
 
         # Transform to proper mean, std
         tensor.mul_(std * math.sqrt(2.0))
